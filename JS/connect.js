939d8655d8987068924cc838ffc71a109df1d401
Arkmandel Esposito <134709542+Esposito-Mr@users.noreply.github.com>
2024-07-02 19:20:54 -0300

    Spinner, better submission handling and connetion

--- JS/connect.js
+++ JS/connect.js
@@ -1,8 +1,7 @@
-function submitForm(event) {
-    event.preventDefault();
-    var form = event.target;
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action, true);
-    xhr.send(formData);
-  } 
+// Google Apps Script to handle form submission and write to Google Sheets
+function doPost(e) {
+  const sheet = SpreadsheetApp.openById('1q7qW1E8T34Z_52ZZ58tdhDvbMqWaQSeFkO-fOSWs1pA').getSheetByName('Sheet1');
+  const data = e.parameter;
+  sheet.appendRow([data.name, data.email]);
+  return ContentService.createTextOutput(JSON.stringify({ 'result': 'success' })).setMimeType(ContentService.MimeType.JSON);
+}
