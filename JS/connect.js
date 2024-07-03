20652e3c9e7346884afdc845f786bf258e8ee886
Arkmandel Esposito <134709542+Esposito-Mr@users.noreply.github.com>
2024-07-02 21:02:54 -0300

    Remove connection to google sheets

--- JS/connect.js
+++
@@ -1,7 +0,0 @@
-// Google Apps Script to handle form submission and write to Google Sheets
-function doPost(e) {
-  const sheet = SpreadsheetApp.openById('1q7qW1E8T34Z_52ZZ58tdhDvbMqWaQSeFkO-fOSWs1pA').getSheetByName('Sheet1');
-  const data = e.parameter;
-  sheet.appendRow([data.name, data.email]);
-  return ContentService.createTextOutput(JSON.stringify({ 'result': 'success' })).setMimeType(ContentService.MimeType.JSON);
-}
