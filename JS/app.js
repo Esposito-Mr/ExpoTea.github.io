21e9f06f11241e1944cc7e2e1dd4233df4762e9f
Arkmandel Esposito <Joaoespo@gmail.com>
2025-06-05 15:16:30 -0300

    New final page addition

--- JS/app.js
+++ JS/app.js
@@ -64,8 +64,13 @@ firebase.auth().signInAnonymously()
         // Send form data to Firestore
         db.collection("formSubmissions").add(formValues)
           .then(() => {
-           console.log("Form data submitted successfully!");
-           alert("Obrigado pela Inscrição!"); // Display a success message to the user
+// 1) Oculta o container de formulário (todas as etapas)
+      const formContainer = document.getElementById("formContainer");
+      formContainer.style.display = "none";
+
+      // 2) Exibe a div de agradecimento
+      const thankYouPage = document.getElementById("thankYouPage");
+      thankYouPage.style.display = "block";
            form.reset(); // Clear the form after successful submission
           })
           .catch((error) => {
@@ -76,6 +81,6 @@ firebase.auth().signInAnonymously()
              alert("An error occurred while submitting the form. Please try again."); // Display a generic error message to the user
            }
           });
-        }, 500); // Delay the execution by 2 seconds (adjust the time as needed)
+        }, 200); // Delay the execution by 2 seconds (adjust the time as needed)
     });
   }
